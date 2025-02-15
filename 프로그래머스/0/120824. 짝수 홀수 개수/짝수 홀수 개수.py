9515f7f299ad47066f49414aa251f20485479bc9
POIZXC12 <lmsuk7554@gmail.com>
2025-02-15 22:27:18 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    a = 0
+    b = 0
+    for i in range(0, len(num_list)):
+        if num_list[i] % 2 == 0:
+            a += 1
+        else:
+            b += 1
+            
+    return a, b
