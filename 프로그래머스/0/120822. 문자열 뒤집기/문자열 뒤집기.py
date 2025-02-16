8e87e912235b4cda4849e70f052eb09278ad0812
POIZXC12 <lmsuk7554@gmail.com>
2025-02-16 17:56:42 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -1,9 +1,7 @@
 def solution(my_string):
     num = ''
     
-    l = len(my_string)
-    
-    for i in range(l - 1, -1, -1):
+    for i in range(len(my_string) - 1, -1, -1):
         num += my_string[i]
         
     return num
