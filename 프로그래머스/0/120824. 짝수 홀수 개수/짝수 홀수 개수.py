05cf12150a02e4bceba83ded174385a31071120c
POIZXC12 <lmsuk7554@gmail.com>
2025-02-16 17:46:29 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.py
@@ -1,8 +1,9 @@
 def solution(num_list):
     a = 0
     b = 0
-    for i in num_list:
-        if i % 2 == 0:
+    
+    for i in range(0, len(num_list)):
+        if num_list[i] % 2 == 0:
             a += 1
         else:
             b += 1
