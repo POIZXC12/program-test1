3d1266a0ceaaf2b392708fdf4a57fb3e10406d0c
POIZXC12 <lmsuk7554@gmail.com>
2025-03-20 00:23:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 176 ms, Memory: 14976 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader라는 매서드(함수)에서 새로운 객체 생성 (br이라는 이름의 입력 함수를 만듦)
+        // 쉽게 생각하면 br이라는 입력받는 기계를 만든거임
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //띄어쓰기로 입력받았을때 띄어쓰기를 기준으로 나눠주는 착한 아이
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
