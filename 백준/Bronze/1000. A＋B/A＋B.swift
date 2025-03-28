52992d6e50c07a6b83c59c99668fbf1f6b8731c2
POIZXC12 <lmsuk7554@gmail.com>
2025-03-28 18:47:19 +0900

    [Bronze V] Title: A+B, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Baekjoon
+//
+//  Created by 이민석 on 3/28/25.
+//
+
+import Foundation
+
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+
+let A = input[0]
+let B = input[1]
+
+print(A + B)
