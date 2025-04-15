fb34eab95c7f0bb5f766ded80ae943a1cf30b442
POIZXC12 <lmsuk7554@gmail.com>
2025-04-16 02:36:28 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        
+        // readLine()이 null을 반환할 때까지 (EOF까지)
+        while ((line = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(line);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a + b);
+        }
+    }
+}
