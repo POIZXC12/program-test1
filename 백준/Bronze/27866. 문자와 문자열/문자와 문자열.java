9ec504fe6183563e60a8864c374826f3b3157ff0
POIZXC12 <lmsuk7554@gmail.com>
2025-04-16 02:41:13 +0900

    [Bronze V] Title: 문자와 문자열, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,16 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // BufferedReader라는 매서드(함수)에서 새로운 객체 생성 (br이라는 이름의 입력 함수를 만듦)
+        // 쉽게 생각하면 br이라는 입력받는 기계를 만든거임
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //띄어쓰기로 입력받았을때 띄어쓰기를 기준으로 나눠주는 착한 아이
+        //StringTokenizer st = new StringTokenizer(br.readLine());
+        String input = br.readLine();
+        int input2 = Integer.parseInt(br.readLine());
+        System.out.println(input.charAt(input2-1));
+    }
+}
